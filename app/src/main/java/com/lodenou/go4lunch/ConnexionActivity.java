ddabb37996066dd0da0b0f93d4a7638dadd12d5e
Lodenou <denoual.loic.dl@gmail.com>
2020-07-03 23:58:30 +0200

    google sign-in done

--- app/src/main/java/com/lodenou/go4lunch/ConnexionActivity.java
+++ app/src/main/java/com/lodenou/go4lunch/ConnexionActivity.java
@@ -1,37 +1,74 @@
 package com.lodenou.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
-
 import java.util.Arrays;
+
 
 
 public class ConnexionActivity extends AppCompatActivity {
 
     // 1 - Identifier for Sign-In Activity
     private static final int RC_SIGN_IN = 123;
+    private final String TAG = "cycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
-        startSignInActivity(); //FIXME à mettre dans un onclick sur le bouton google
+        startSignInActivity(); //FIXME
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+        startSignInActivity();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume: ");
     }
 
     private void startSignInActivity(){
+
         startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setTheme(R.style.LoginTheme)
-                        .setAvailableProviders(
-                                Arrays.asList(
-                                        new AuthUI.IdpConfig.GoogleBuilder().build())) //GOOGLE
-//                                        new AuthUI.IdpConfig.FacebookBuilder().build())) // FACEBOOK
-                        .setIsSmartLockEnabled(false, true)
-                        .setLogo(R.drawable.trybowlsoup)
-                        .build(),
-                RC_SIGN_IN);
+        AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.LoginTheme)
+                .setLogo(R.drawable.bowlofsouplogotriangle)
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.GoogleBuilder().build(),
+                        new AuthUI.IdpConfig.FacebookBuilder().build()))
+                .setIsSmartLockEnabled(false).build(), RC_SIGN_IN);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+
+        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+        homeIntent.addCategory( Intent.CATEGORY_HOME );
+        homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
