cf1d92687d7021c6440215aa5ba42f58ee3e114a
Lodenou <denoual.loic.dl@gmail.com>
2020-07-18 14:55:40 +0200

    Created navigationdrawer activity

--- app/src/main/java/com/lodenou/go4lunch/ConnexionActivity.java
+++ app/src/main/java/com/lodenou/go4lunch/ConnexionActivity.java
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import java.util.Arrays;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.shobhitpuri.custombuttons.GoogleSignInButton;
 
 
 
@@ -17,48 +26,80 @@ public class ConnexionActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
     private final String TAG = "cycle";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getSupportActionBar().hide();
         setContentView(R.layout.activity_connexion);
-        startSignInActivity(); //FIXME
+        GoogleSignInButton mSignInButton = findViewById(R.id.btn_sign_in);
+        googleBtnOnClick();
     }
 
     @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart: ");
-        startSignInActivity();
+    protected void onStart() {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        super.onStart();
+    }
+
+    private void googleBtnOnClick() {
+        GoogleSignInButton mSignInButton = findViewById(R.id.btn_sign_in);
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_sign_in:
+                        signIn();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void signIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG,"onResume: ");
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
 
-    private void startSignInActivity(){
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
-        startActivityForResult(
-        AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setTheme(R.style.LoginTheme)
-                .setLogo(R.drawable.bowlofsouplogotriangle)
-                .setAvailableProviders(Arrays.asList(
-                        new AuthUI.IdpConfig.GoogleBuilder().build(),
-                        new AuthUI.IdpConfig.FacebookBuilder().build()))
-                .setIsSmartLockEnabled(false).build(), RC_SIGN_IN);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+//            Intent intent = new Intent(ConnexionActivity.this, MainActivity.class);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
     }
 
-    @Override
-    public void onBackPressed() {
-
-
-        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
-        homeIntent.addCategory( Intent.CATEGORY_HOME );
-        homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(homeIntent);
-
+    public void updateUI(GoogleSignInAccount account) {
+        if (account != null) {
+            Toast.makeText(this, "You successfully signed-in ", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            Toast.makeText(this, "Failed to sign-in", Toast.LENGTH_LONG).show();
+        }
     }
 }
 
