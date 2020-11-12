df41e30715a1a7e04565dadf1a72d863fd45d24c
Lodenou <denoual.loic.dl@gmail.com>
2020-11-12 15:27:48 +0100

    Creating Restaurant model

---
+++ app/src/main/java/com/lodenou/go4lunch/model/Restaurant.java
@@ -0,0 +1,14 @@
+package com.lodenou.go4lunch.model;
+
+public class Restaurant {
+
+    private String id;
+    private String name;
+
+    // DEFAULT CONSTRUCTOR
+    public Restaurant(){}
+
+
+
+
+}
