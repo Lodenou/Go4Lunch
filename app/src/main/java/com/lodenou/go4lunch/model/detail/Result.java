2110aff095a8507883a4cece7c002f35c833fc46
Lodenou <denoual.loic.dl@gmail.com>
2021-01-13 09:31:09 +0100

    Result created

---
+++ app/src/main/java/com/lodenou/go4lunch/model/detail/Result.java
@@ -0,0 +1,65 @@
+package com.lodenou.go4lunch.model.detail;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Result {
+
+    @SerializedName("international_phone_number")
+    @Expose
+    private String internationalPhoneNumber;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("photos")
+    @Expose
+    private List<Photo> photos = null;
+    @SerializedName("vicinity")
+    @Expose
+    private String vicinity;
+    @SerializedName("website")
+    @Expose
+    private String website;
+
+    public String getInternationalPhoneNumber() {
+        return internationalPhoneNumber;
+    }
+
+    public void setInternationalPhoneNumber(String internationalPhoneNumber) {
+        this.internationalPhoneNumber = internationalPhoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public String getVicinity() {
+        return vicinity;
+    }
+
+    public void setVicinity(String vicinity) {
+        this.vicinity = vicinity;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+}
