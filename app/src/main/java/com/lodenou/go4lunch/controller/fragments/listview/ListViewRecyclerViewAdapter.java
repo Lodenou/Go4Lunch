989b2a7a2730c5e5b1e0dc5e5fbc27b0c69ddbae
Lodenou <denoual.loic.dl@gmail.com>
2021-01-15 12:43:23 +0100

    listview 50% set

--- app/src/main/java/com/lodenou/go4lunch/controller/fragments/listview/ListViewRecyclerViewAdapter.java
+++ app/src/main/java/com/lodenou/go4lunch/controller/fragments/listview/ListViewRecyclerViewAdapter.java
@@ -3,23 +3,33 @@ package com.lodenou.go4lunch.controller.fragments.listview;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.lodenou.go4lunch.R;
 import com.lodenou.go4lunch.model.nearbysearch.Result;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 public class ListViewRecyclerViewAdapter extends RecyclerView.Adapter<ListViewViewHolder> {
 
 
     private List<Result> mRestaurants;
+    Context context;
 
     public ListViewRecyclerViewAdapter(List<Result> restaurants) {
         this.mRestaurants = restaurants;
+
     }
 
 
@@ -35,16 +45,38 @@ public class ListViewRecyclerViewAdapter extends RecyclerView.Adapter<ListViewVi
         return viewHolder;
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ListViewViewHolder holder, int position) {
         Result restaurant = mRestaurants.get(position);
 
-        TextView textView = holder.mContentView;
-        TextView textView1 = holder.mIdView;
+        TextView restaurantName = holder.mContentView;
+        TextView restaurantAddress = holder.mRestaurantAddress;
+        TextView openingHours = holder.mOpeningHours;
+        TextView distance = holder.mDistance;
+        TextView workmatesNumber = holder.mWorkmatesNumber;
+        TextView ratingStars = holder.mRatingStars;
+        ImageView restaurantImage = holder.mRestaurantImage;
 
-        textView.setText(restaurant.getName());
+
+        restaurantName.setText(restaurant.getName());
+        restaurantAddress.setText(restaurant.getVicinity());
+        openingHours.setText(restaurant.getBusinessStatus());
+        //TODO distance à calculer
+        distance.setText(restaurant.getScope());
+        //TODO workmatesnumber
+        workmatesNumber.setText(restaurant.getUserRatingsTotal().toString());
+        ratingStars.setText(restaurant.getRating().toString());
+
+        //TODO image du restaurant
+//        restaurantImage.setImageBitmap(restaurant.getPhotos().get(0).getPhotoReference());
+//        Glide.with(context).load(restaurant.getIcon()).into(restaurantImage);
+
+
+
         //TODO getimage etc
-        textView1.setText(String.valueOf(position));
+        //TODO onclick
+//        textView1.setText(String.valueOf(position));
     }
 
 
--- app/src/main/java/com/lodenou/go4lunch/controller/fragments/listview/ListViewViewHolder.java
+++ app/src/main/java/com/lodenou/go4lunch/controller/fragments/listview/ListViewViewHolder.java
@@ -3,6 +3,7 @@ package com.lodenou.go4lunch.controller.fragments.listview;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,15 +14,30 @@ import com.lodenou.go4lunch.controller.activitiy.yourlunchactivity.YourLunchActi
 
 public class ListViewViewHolder extends RecyclerView.ViewHolder {
 
-    public final TextView mIdView;
+
     public final TextView mContentView;
+    public final TextView mRestaurantAddress;
+    public final TextView mOpeningHours;
+    public final TextView mDistance;
+    public final TextView mWorkmatesNumber;
+    public final TextView mRatingStars;
+    public final ImageView mRestaurantImage;
 
 
     public ListViewViewHolder(@NonNull View itemView) {
         super(itemView);
 
-        mIdView = (TextView) itemView.findViewById(R.id.item_number);
-        mContentView = (TextView) itemView.findViewById(R.id.content);
+
+        mContentView = (TextView) itemView.findViewById(R.id.restaurant_name);
+        mRestaurantAddress = itemView.findViewById(R.id.restaurant_address);
+        mOpeningHours = itemView.findViewById(R.id.opening_hours);
+        mDistance = itemView.findViewById(R.id.distance);
+        mWorkmatesNumber = itemView.findViewById(R.id.workmates_number);
+        mRatingStars = itemView.findViewById(R.id.rating_stars);
+        mRestaurantImage = itemView.findViewById(R.id.restaurant_image);
+
+
+
         startLunchActivityOnClick();
     }
 
@@ -30,8 +46,11 @@ public class ListViewViewHolder extends RecyclerView.ViewHolder {
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Context context = v.getContext();
                 Intent intent = new Intent(context, YourLunchActivity.class);
+                //TODO envoyer le nom du restaurant selectionné dans la liste
+//                intent.putExtra("key", res)
                 context.startActivity(intent);
             }
         });
