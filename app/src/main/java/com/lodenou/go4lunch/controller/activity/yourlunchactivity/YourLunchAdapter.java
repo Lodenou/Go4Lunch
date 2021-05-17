9b01b79503dad286100844c9f6a35abb5b592e30
Lodenou <denoual.loic.dl@gmail.com>
2021-05-18 01:23:08 +0200

    The list of users for YourlunchActivity is ok

--- app/src/main/java/com/lodenou/go4lunch/controller/activity/yourlunchactivity/YourLunchAdapter.java
+++ app/src/main/java/com/lodenou/go4lunch/controller/activity/yourlunchactivity/YourLunchAdapter.java
@@ -1,13 +1,17 @@
 package com.lodenou.go4lunch.controller.activity.yourlunchactivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.lodenou.go4lunch.R;
 import com.lodenou.go4lunch.model.User;
 
@@ -43,14 +47,33 @@ public class YourLunchAdapter extends RecyclerView.Adapter<YourLunchViewHolder> 
 
 
 
-//        TextView usernameLunch = holder.mUsernameLunch;
-//        // change text color to black to fit with the white bg
-//        usernameLunch.setTextColor(Color.argb(255, 0, 0, 0));
-//        textView.setText(user.getName());
-//        TextView textView1 = holder.mIdView;
-//        textView1.setText(String.valueOf(position));
+        TextView usernameLunch = holder.mUsernameLunch;
         // change text color to black to fit with the white bg
-//        textView1.setTextColor(Color.argb(255, 0, 0, 0));
+        usernameLunch.setTextColor(Color.argb(255, 0, 0, 0));
+        usernameLunch.setText(user.getName());
+        TextView userChoice = holder.mUserChoice;
+        userChoice.setText(" is joining !");
+//         change text color to black to fit with the white bg
+        userChoice.setTextColor(Color.argb(255, 0, 0, 0));
+
+        ImageView userAvatar = holder.mAvatarUserLunch;
+        String avatarUrl = user.getAvatarUrl();
+        if (avatarUrl != null) {
+            Context context = userAvatar.getContext();
+            Glide.with(context)
+                    .load(avatarUrl)
+                    .sizeMultiplier(0.08f)
+                    .circleCrop()
+                    .into(userAvatar);
+        }
+        else {
+            Context context = userAvatar.getContext();
+            Glide.with(context)
+                    .load("https://fnadepape.org/wp-content/uploads/2018/07/avatar-1577909_960_720.png")
+                    .sizeMultiplier(0.08f)
+                    .circleCrop()
+                    .into(userAvatar);
+        }
     }
 
     @Override
