163102b8771d96776e1947566665ac6907377e98
Lodenou <denoual.loic.dl@gmail.com>
2021-05-20 23:25:12 +0200

    Chatadapter 1st commit

---
+++ app/src/main/java/com/lodenou/go4lunch/controller/activity/chatactivity/ChatAdapter.java
@@ -0,0 +1,52 @@
+package com.lodenou.go4lunch.controller.activity.chatactivity;
+
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.RequestManager;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.lodenou.go4lunch.R;
+
+public class ChatAdapter extends FirestoreRecyclerAdapter<Message, MessageViewHolder> {
+
+
+    public interface Listener {
+            void onDataChanged();
+        }
+
+        //FOR DATA
+        private final RequestManager glide;
+        private final String idCurrentUser;
+
+        //FOR COMMUNICATION
+        private Listener callback;
+
+        public ChatAdapter(@NonNull FirestoreRecyclerOptions<Message> options, RequestManager glide, Listener callback, String idCurrentUser) {
+            super(options);
+            this.glide = glide;
+            this.callback = callback;
+            this.idCurrentUser = idCurrentUser;
+        }
+
+        @Override
+        protected void onBindViewHolder(@NonNull MessageViewHolder holder, int position, @NonNull Message model) {
+            holder.updateWithMessage(model, this.idCurrentUser, this.glide);
+        }
+
+        @Override
+        public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MessageViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.activity_chat_item, parent, false));
+        }
+
+        @Override
+        public void onDataChanged() {
+            super.onDataChanged();
+            this.callback.onDataChanged();
+        }
+    }
