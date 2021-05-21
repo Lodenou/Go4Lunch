75272b5ba57fd775c1e34cc2f7e206b5e4cacb51
Lodenou <denoual.loic.dl@gmail.com>
2021-05-21 22:14:28 +0200

    createMessageForChat

--- app/src/main/java/com/lodenou/go4lunch/controller/api/MessageHelper.java
+++ app/src/main/java/com/lodenou/go4lunch/controller/api/MessageHelper.java
@@ -1,7 +1,13 @@
 package com.lodenou.go4lunch.controller.api;
 
 
+
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.Query;
+import com.lodenou.go4lunch.model.Message;
+import com.lodenou.go4lunch.model.User;
 
 public class MessageHelper {
 
@@ -15,5 +21,17 @@ public class MessageHelper {
                 .collection(COLLECTION_NAME)
                 .orderBy("dateCreated")
                 .limit(50);
+    }
+
+    public static Task<DocumentReference> createMessageForChat(String textMessage, String chat, User userSender){
+
+        // 1 - Create the Message object
+        Message message = new Message(textMessage, userSender);
+
+        // 2 - Store Message to Firestore
+        return ChatHelper.getChatCollection()
+                .document(chat)
+                .collection(COLLECTION_NAME)
+                .add(message);
     }
 }
