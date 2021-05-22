cae9935929df98b0cb623458f821de4c485a9782
Lodenou <denoual.loic.dl@gmail.com>
2021-05-22 22:40:10 +0200

    cleaning + try on new configureRecyclerView()

--- app/src/main/java/com/lodenou/go4lunch/controller/activity/chatactivity/ChatActivity.java
+++ app/src/main/java/com/lodenou/go4lunch/controller/activity/chatactivity/ChatActivity.java
@@ -28,16 +28,18 @@ import com.lodenou.go4lunch.controller.api.UserHelper;
 import com.lodenou.go4lunch.model.Message;
 import com.lodenou.go4lunch.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class ChatActivity extends AppCompatActivity implements ChatAdapter.Listener {
 
-//    public class MentorChatActivity extends BaseActivity implements MentorChatAdapter.Listener {
 
         // FOR DESIGN
         // 1 - Getting all views needed
-        @BindView(R.id.activity_mentor_chat_recycler_view) RecyclerView recyclerView;
+        @BindView(R.id.activity_chat_recycler_view) RecyclerView mRecyclerView;
         @BindView(R.id.activity_mentor_chat_text_view_recycler_view_empty) TextView textViewRecyclerViewEmpty;
         @BindView(R.id.activity_mentor_chat_message_edit_text) TextInputEditText editTextMessage;
         @BindView(R.id.activity_mentor_chat_image_chosen_preview) ImageView imageViewPreview;
@@ -48,6 +50,7 @@ public class ChatActivity extends AppCompatActivity implements ChatAdapter.Liste
         @Nullable
         private User modelCurrentUser;
         private String currentChatName;
+        private List<Message> mMessages;
 
         // STATIC DATA FOR CHAT (3)
         private static final String CHAT_NAME_ANDROID = "android";
@@ -57,8 +60,8 @@ public class ChatActivity extends AppCompatActivity implements ChatAdapter.Liste
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
-            this.configureRecyclerView(CHAT_NAME_ANDROID);
+            setContentView(R.layout.activity_chat);
+            this.configureRecyclerView();
 //            this.configureToolbar();
             this.getCurrentUserFromFirestore();
         }
@@ -86,21 +89,21 @@ public class ChatActivity extends AppCompatActivity implements ChatAdapter.Liste
         }
     }
 
-        @OnClick({ R.id.activity_mentor_chat_android_chat_button, R.id.activity_mentor_chat_firebase_chat_button, R.id.activity_mentor_chat_bug_chat_button})
-        public void onClickChatButtons(ImageButton imageButton) {
-            // 8 - Re-Configure the RecyclerView depending chosen chat
-            switch (Integer.valueOf(imageButton.getTag().toString())){
-                case 10:
-                    this.configureRecyclerView(CHAT_NAME_ANDROID);
-                    break;
-                case 20:
-                    this.configureRecyclerView(CHAT_NAME_FIREBASE);
-                    break;
-                case 30:
-                    this.configureRecyclerView(CHAT_NAME_BUG);
-                    break;
-            }
-        }
+//        @OnClick({ R.id.activity_mentor_chat_android_chat_button, R.id.activity_mentor_chat_firebase_chat_button, R.id.activity_mentor_chat_bug_chat_button})
+//        public void onClickChatButtons(ImageButton imageButton) {
+//            // 8 - Re-Configure the RecyclerView depending chosen chat
+//            switch (Integer.valueOf(imageButton.getTag().toString())){
+//                case 10:
+//                    this.configureRecyclerView(CHAT_NAME_ANDROID);
+//                    break;
+//                case 20:
+//                    this.configureRecyclerView(CHAT_NAME_FIREBASE);
+//                    break;
+//                case 30:
+//                    this.configureRecyclerView(CHAT_NAME_BUG);
+//                    break;
+//            }
+//        }
 
         @OnClick(R.id.activity_mentor_chat_add_file_button)
         public void onClickAddFile() { }
@@ -123,24 +126,38 @@ public class ChatActivity extends AppCompatActivity implements ChatAdapter.Liste
         // UI
         // --------------------
         // 5 - Configure RecyclerView with a Query
-        private void configureRecyclerView(String chatName){
-            //Track current chat name
-            this.currentChatName = chatName;
-            //Configure Adapter & RecyclerView
-            this.mChatAdapter = new ChatAdapter(generateOptionsForAdapter(MessageHelper.getAllMessageForChat(this.currentChatName)),
+//        private void configureRecyclerView(String chatName){
+//            //Track current chat name
+//            this.currentChatName = chatName;
+//            //Configure Adapter & RecyclerView
+//            this.mChatAdapter = new ChatAdapter(generateOptionsForAdapter(MessageHelper.getAllMessageForChat(this.currentChatName)),
+//                    Glide.with(this),
+//                    this,
+//                    FirebaseAuth.getInstance().getCurrentUser()
+//                            .getUid());
+//            mChatAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+//                @Override
+//                public void onItemRangeInserted(int positionStart, int itemCount) {
+//                    recyclerView.smoothScrollToPosition(mChatAdapter.getItemCount()); // Scroll to bottom on new messages
+//                }
+//            });
+//            recyclerView.setLayoutManager(new LinearLayoutManager(this.getApplicationContext()));
+//            recyclerView.setAdapter(this.mChatAdapter);
+//        }
+
+    private void configureRecyclerView() {
+
+        this.mMessages = new ArrayList<>();
+        this.mChatAdapter = new ChatAdapter(generateOptionsForAdapter(MessageHelper.getAllMessageForChat(this.currentChatName)),
                     Glide.with(this),
                     this,
                     FirebaseAuth.getInstance().getCurrentUser()
                             .getUid());
-            mChatAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-                @Override
-                public void onItemRangeInserted(int positionStart, int itemCount) {
-                    recyclerView.smoothScrollToPosition(mChatAdapter.getItemCount()); // Scroll to bottom on new messages
-                }
-            });
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-            recyclerView.setAdapter(this.mChatAdapter);
-        }
+        mRecyclerView.setAdapter(mChatAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
 
         // 6 - Create options for RecyclerView from a Query
         private FirestoreRecyclerOptions<Message> generateOptionsForAdapter(Query query){
