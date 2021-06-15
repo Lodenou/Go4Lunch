4292018246eaa514f70e8b9e6fb60b4674e7bdaa
Lodenou <denoual.loic.dl@gmail.com>
2021-06-15 12:38:48 +0200

    Update detail class

--- app/src/main/java/com/lodenou/go4lunch/model/detail/Result.java
+++ app/src/main/java/com/lodenou/go4lunch/model/detail/Result.java
@@ -4,23 +4,44 @@ import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+
 public class Result {
 
+    @SerializedName("geometry")
+    @Expose
+    private Geometry geometry;
     @SerializedName("international_phone_number")
     @Expose
     private String internationalPhoneNumber;
     @SerializedName("name")
     @Expose
     private String name;
+    @SerializedName("opening_hours")
+    @Expose
+    private OpeningHours openingHours;
     @SerializedName("photos")
     @Expose
     private List<Photo> photos = null;
+    @SerializedName("rating")
+    @Expose
+    private Double rating;
     @SerializedName("vicinity")
     @Expose
     private String vicinity;
     @SerializedName("website")
     @Expose
     private String website;
+    @SerializedName("place_id")
+    @Expose
+    private String placeId;
+
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
 
     public String getInternationalPhoneNumber() {
         return internationalPhoneNumber;
@@ -38,12 +59,28 @@ public class Result {
         this.name = name;
     }
 
+    public OpeningHours getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(OpeningHours openingHours) {
+        this.openingHours = openingHours;
+    }
+
     public List<Photo> getPhotos() {
         return photos;
     }
 
     public void setPhotos(List<Photo> photos) {
         this.photos = photos;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
     }
 
     public String getVicinity() {
@@ -62,4 +99,11 @@ public class Result {
         this.website = website;
     }
 
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
 }
